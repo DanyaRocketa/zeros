56775c4ea876502df717abb67e9c8639625d7eca
Danya_Rocketa <hospodi322@gmail.com>
2021-11-10 03:26:29 +0300

    all tests are passed and the job is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let fiveIterations = Math.floor(number/5);
+  let answer = fiveIterations;
+  let additionalFives = 0;
+  if(fiveIterations >= 5){
+    while(Math.floor(fiveIterations/5) > 0){
+      additionalFives += Math.floor(fiveIterations / 5);
+      fiveIterations = Math.floor(fiveIterations / 5);
+    }
+  }
+  answer += additionalFives; 
+  return answer;
 }
